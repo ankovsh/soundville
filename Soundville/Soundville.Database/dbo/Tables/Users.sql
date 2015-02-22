321778cd8a5bbd1c6de9caf81fd8118a08347854
ankovsh <anastasya.ermoshina@gmail.com>
2015-02-22 17:15:40 +0300

    Adding database project.

---
+++ Soundville/Soundville.Database/dbo/Tables/Users.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Users] (
+    [Id]           INT            IDENTITY (1, 1) NOT NULL,
+    [UserName]     NVARCHAR (50)  NOT NULL,
+    [PasswordHash] NVARCHAR (MAX) NULL,
+    [FirstName]	   NVARCHAR (50)  NOT NULL,
+    CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
--- Soundville/Soundville.Domain/Models/User.cs
+++ Soundville/Soundville.Domain/Models/User.cs
@@ -6,6 +6,7 @@ namespace Soundville.Domain.Models
     {
         public int Id { get; set; }
         public string UserName { get; set; }
+        public string FirstName { get; set; }
         public string PasswordHash { get; set; }
     }
 }
