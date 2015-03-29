c178320e873439ce318a72f166e34ed83c08c2c8
ankovsh <anastasya.ermoshina@gmail.com>
2015-03-30 01:28:44 +0300

    View song of search page.

--- Soundville/Soundville.Web/Scripts/Custom/Search.js
+++ Soundville/Soundville.Web/Scripts/Custom/Search.js
@@ -9,8 +9,16 @@
 
                 var items = data.Items;
                 for (var i = 0; i < items.length; i++) {
-                    var li = $("<li>").addClass("song").attr("data-id", items[i].Id).text(items[i].Artist + " - " + items[i].Title);
+                    var li = $("<li>").addClass("song");
                     $("#search-result").append(li);
+                    var a = $("<a>").addClass("song-link")
+                                    .attr("data-id", items[i].Id)
+                                    .text(items[i].Artist + " - " + items[i].Title);
+                    li.append(a);
+                    var audio = $("<audio>").attr("src", items[i].Url);
+                    li.append(audio);
+                    var hr = $("<hr/>");
+                    $("#search-result").append(hr);
                 }
             }
         });
@@ -18,5 +26,23 @@
 
     $(document).on("click", ".song", function () {
         console.log($(this).data("id"));
+    });
+
+    $(document).on("click", ".song-link", function () {
+        var wavesurfer = Object.create(WaveSurfer);
+
+        wavesurfer.init({
+            container: document.querySelector('#wave'),
+            waveColor: 'violet',
+            progressColor: 'purple'
+        });
+
+        wavesurfer.on('ready', function() {
+            wavesurfer.play();
+        });
+
+        var songUrl = $(this).next().attr("src");
+
+        wavesurfer.load(songUrl);
     });
 });
